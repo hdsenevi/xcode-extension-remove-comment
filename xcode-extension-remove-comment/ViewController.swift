03d9677cbcb127423685e0a297d3968b3ec99182
Sha Senevirathne <sha.senevirathne@ofx.com>
2017-07-05 16:10:29 +1000

    Initial Commit

---
+++ xcode-extension-remove-comment/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode-extension-remove-comment
+//
+//  Created by Sha on 5/7/17.
+//  Copyright © 2017 Sha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
