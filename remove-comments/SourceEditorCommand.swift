6e1c8851a9e495e0b781bf3fa1fd67907bd05d01
Shanaka Senevirathne <shanaka36@gmail.com>
2017-07-06 10:39:33 +1000

    Also removed the top comment lines

--- remove-comments/SourceEditorCommand.swift
+++ remove-comments/SourceEditorCommand.swift
@@ -17,6 +17,7 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         print("Content Type : \(invocation.buffer.contentUTI)")
         print("Command Identifier : \(invocation.commandIdentifier)")
         
+        // Replace top header with the preset text
         if invocation.commandIdentifier.contains("TestHeader") {
             var reachedEndOfHeader = false
             var lastCommentLine = 0
@@ -35,8 +36,16 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
                 }
             }
             
+            // Remove top comment
+            invocation.buffer.lines.removeObjects(in: NSRange(location: 0, length: lastCommentLine))
+            
             // Assign the new header
-            invocation.buffer.lines[0] = newHeader
+            invocation.buffer.lines.insert(newHeader, at: 0)
+        }
+        
+        // Remove all comments command
+        if invocation.commandIdentifier.contains("RemoveComments") {
+            
         }
         
         completionHandler(nil)
